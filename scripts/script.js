ed53851b2a82f8e69985cfbc29d5a02f67cacf18
acnologia <shrikanthbudya@gmail.com>
2018-04-13 23:47:56 +0530

    Added lerping.

--- scripts/script.js
+++ scripts/script.js
@@ -4,12 +4,12 @@ var ctx = can.getContext("2d");
 var arr = [];
 var grid = [];
 const TOTAL_CIRCLES = 10;
-const GRID_SIZE = 7.5;
+const GRID_SIZE = 10;
 class Circle {
     constructor() {
-        this.x = ~~(Math.random() * can.width);
-        this.y = ~~(Math.random() * can.height);
-        this.radius = 20 + ~~(Math.random() * 50);
+        this.x = 50 + ~~(Math.random() * (can.width - 100));
+        this.y = 50 + ~~(Math.random() * (can.height - 100));
+        this.radius = 10 + ~~(Math.random() * 50);
         this.directions = [1, -1]
         this.vel = {
             x: 3 * Math.random() * this.directions[~~(Math.random() * 2)],
@@ -23,12 +23,6 @@ class Circle {
             this.vel.x *= -1;
         if (this.y + this.radius >= can.height || this.y - this.radius <= 0)
             this.vel.y *= -1;
-    }
-    draw() {
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI * 2);
-        //ctx.stroke();
-        ctx.closePath();
     }
 }
 
@@ -44,11 +38,16 @@ function setup() {
         }
     }
 }
+function lerp(p1, p2, g) {
+    return {
+        x: p2.x + (p1.x - p2.x) * ((1 - g[p2.x / GRID_SIZE][p2.y / GRID_SIZE]) / (g[p1.x / GRID_SIZE][p1.y / GRID_SIZE] - g[p2.x / GRID_SIZE][p2.y / GRID_SIZE])),
+        y: p2.y + (p1.y - p2.y) * ((1 - g[p2.x / GRID_SIZE][p2.y / GRID_SIZE]) / (g[p1.x / GRID_SIZE][p1.y / GRID_SIZE] - g[p2.x / GRID_SIZE][p2.y / GRID_SIZE]))
+    }
+}
 function draw() {
     ctx.clearRect(0, 0, can.width, can.height);
     for (var i = 0; i < TOTAL_CIRCLES; ++i) {
         arr[i].update();
-        arr[i].draw();
     }
     for (var i = 0; i < can.width; i += GRID_SIZE) {
         for (var j = 0; j < can.height; j += GRID_SIZE) {
@@ -56,80 +55,86 @@ function draw() {
             for (var k = 0; k < TOTAL_CIRCLES; ++k) {
                 v += (arr[k].radius * arr[k].radius) / (((arr[k].x - i) * (arr[k].x - i)) + ((arr[k].y - j) * (arr[k].y - j)))
             }
-            if (v > 1) {
-                ctx.beginPath();
-                ctx.arc(i, j, 2, 0, Math.PI * 2);
-                //ctx.fill();
-                ctx.closePath();
-            }
             grid[(i / GRID_SIZE)][(j / GRID_SIZE)] = v;
         }
     }
+    var threshold = 1.0;
     for (var i = 0; i < can.width - GRID_SIZE; i += GRID_SIZE) {
         for (var j = 0; j < can.height - GRID_SIZE; j += GRID_SIZE) {
-            var a = grid[i / GRID_SIZE][j / GRID_SIZE] > 1;
-            var b = grid[i / GRID_SIZE][(j + GRID_SIZE) / GRID_SIZE] > 1;
-            var c = grid[(i + GRID_SIZE) / GRID_SIZE][(j + GRID_SIZE) / GRID_SIZE] > 1;
-            var d = grid[(i + GRID_SIZE) / GRID_SIZE][j / GRID_SIZE] > 1;
+            var a = grid[i / GRID_SIZE][j / GRID_SIZE] > threshold;
+            var b = grid[i / GRID_SIZE][(j + GRID_SIZE) / GRID_SIZE] > threshold;
+            var c = grid[(i + GRID_SIZE) / GRID_SIZE][(j + GRID_SIZE) / GRID_SIZE] > threshold;
+            var d = grid[(i + GRID_SIZE) / GRID_SIZE][j / GRID_SIZE] > threshold;
+            var ap = {
+                x: i,
+                y: j
+            }, bp = {
+                x: i,
+                y: j + GRID_SIZE
+            }, cp = {
+                x: i + GRID_SIZE,
+                y: j + GRID_SIZE
+            }, dp = {
+                x: i + GRID_SIZE,
+                y: j
+            };
             var p1, p2;
             if ((a && !b && !c && !d) || (!a && b && c && d)) {
                 p1 = {
-                    x: i + GRID_SIZE / 2,
-                    y: j
+                    x: lerp(ap, dp, grid).x,
+                    y: lerp(ap, dp, grid).y
                 };
-
                 p2 = {
-                    x: i,
-                    y: j + GRID_SIZE / 2
+                    x: lerp(ap, bp, grid).x,
+                    y: lerp(ap, bp, grid).y
                 };
             }
             else if ((!a && b && !c && !d) || (a && !b && c && d)) {
                 p1 = {
-                    x: i,
-                    y: j + GRID_SIZE / 2
+                    x: lerp(bp, ap, grid).x,
+                    y: lerp(bp, ap, grid).y
                 };
-
                 p2 = {
-                    x: i + GRID_SIZE / 2,
-                    y: j + GRID_SIZE
+                    x: lerp(bp, cp, grid).x,
+                    y: lerp(bp, cp, grid).y
                 };
             }
             else if ((!a && !b && c && !d) || (a && b && !c && d)) {
                 p1 = {
-                    x: i + GRID_SIZE / 2,
-                    y: j + GRID_SIZE
-                }
+                    x: lerp(cp, bp, grid).x,
+                    y: lerp(cp, bp, grid).y
+                };
                 p2 = {
-                    x: i + GRID_SIZE,
-                    y: j + GRID_SIZE / 2
+                    x: lerp(cp, dp, grid).x,
+                    y: lerp(cp, dp, grid).y
                 };
             }
             else if ((!a && !b && !c && d) || (a && b && c && !d)) {
                 p1 = {
-                    x: i + GRID_SIZE,
-                    y: j + GRID_SIZE / 2
-                }
+                    x: lerp(dp, cp, grid).x,
+                    y: lerp(dp, cp, grid).y
+                };
                 p2 = {
-                    x: i + GRID_SIZE / 2,
-                    y: j
+                    x: lerp(dp, ap, grid).x,
+                    y: lerp(dp, ap, grid).y
                 };
             } else if ((a && b && !c && !d) || (!a && !b && c && d)) {
                 p1 = {
-                    x: i + GRID_SIZE / 2,
-                    y: j
-                }
+                    x: lerp(ap, dp, grid).x,
+                    y: lerp(ap, dp, grid).y
+                };
                 p2 = {
-                    x: i + GRID_SIZE / 2,
-                    y: j + GRID_SIZE
+                    x: lerp(bp, cp, grid).x,
+                    y: lerp(bp, cp, grid).y
                 };
             } else if ((a && !b && !c && d) || (!a && b && c && !d)) {
                 p1 = {
-                    x: i,
-                    y: j + GRID_SIZE / 2
-                }
+                    x: lerp(ap, bp, grid).x,
+                    y: lerp(ap, bp, grid).y
+                };
                 p2 = {
-                    x: i + GRID_SIZE,
-                    y: j + GRID_SIZE / 2
+                    x: lerp(dp, cp, grid).x,
+                    y: lerp(dp, cp, grid).y
                 };
             }
             if (p1 != undefined && p2 != undefined) {
