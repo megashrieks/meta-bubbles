aece81b8ddb0805951d9ed8e63075117090c249f
acnologia <shrikanthbudya@gmail.com>
2018-04-14 14:02:57 +0530

    added dat gui.

--- scripts/script.js
+++ scripts/script.js
@@ -5,11 +5,19 @@ var arr = [];
 var grid = [];
 const drag = 75;
 var settings = {
-    GRID_SIZE: 5,
-    TOTAL_CIRCLES: 35,
+    "grid size": 5,
+    circles: 35,
     delay: 50,
-    show: true
+    show: false
 }, settings_copy = {};
+function gui() {
+    var g = new dat.GUI();
+    g.add(settings, "grid size", 3, 30).step(1);
+    g.add(settings, "circles", 1, 100).step(1);
+    g.add(settings, "delay", 1, 1000).step(1);
+    g.add(settings, "show");
+}
+gui();
 class Circle {
     constructor() {
         this.x = 50 + ~~(Math.random() * (can.width - 100));
@@ -43,21 +51,21 @@ class Circle {
 function setup() {
     // arr = [];
     grid = [];
-    for (var i = arr.length; i < settings.TOTAL_CIRCLES; ++i) {
+    for (var i = arr.length; i < settings.circles; ++i) {
         var circ = new Circle();
         arr.push(circ);
     }
-    for (var i = 0; i < can.width; i += settings.GRID_SIZE) {
+    for (var i = 0; i < can.width; i += settings["grid size"]) {
         grid.push([]);
-        for (var j = 0; j < can.height; j += settings.GRID_SIZE) {
-            grid[i / settings.GRID_SIZE].push(0.0);
+        for (var j = 0; j < can.height; j += settings["grid size"]) {
+            grid[i / settings["grid size"]].push(0.0);
         }
     }
 }
 function lerp(p1, p2) {
     return {
-        x: p2.x + (p1.x - p2.x) * ((1.0 - grid[p2.x / settings.GRID_SIZE][p2.y / settings.GRID_SIZE]) / (grid[p1.x / settings.GRID_SIZE][p1.y / settings.GRID_SIZE] - grid[p2.x / settings.GRID_SIZE][p2.y / settings.GRID_SIZE])),
-        y: p2.y + (p1.y - p2.y) * ((1.0 - grid[p2.x / settings.GRID_SIZE][p2.y / settings.GRID_SIZE]) / (grid[p1.x / settings.GRID_SIZE][p1.y / settings.GRID_SIZE] - grid[p2.x / settings.GRID_SIZE][p2.y / settings.GRID_SIZE]))
+        x: p2.x + (p1.x - p2.x) * ((1.0 - grid[p2.x / settings["grid size"]][p2.y / settings["grid size"]]) / (grid[p1.x / settings["grid size"]][p1.y / settings["grid size"]] - grid[p2.x / settings["grid size"]][p2.y / settings["grid size"]])),
+        y: p2.y + (p1.y - p2.y) * ((1.0 - grid[p2.x / settings["grid size"]][p2.y / settings["grid size"]]) / (grid[p1.x / settings["grid size"]][p1.y / settings["grid size"]] - grid[p2.x / settings["grid size"]][p2.y / settings["grid size"]]))
     }
 }
 function v_to_b(v1, v2, v3, v4) {
@@ -65,181 +73,183 @@ function v_to_b(v1, v2, v3, v4) {
     return v4 + (v3 << 1) + (v2 << 2) + (v1 << 3);
 }
 function draw() {
-    if (JSON.stringify(settings_copy) != JSON.stringify(settings)) {
-        setup();
-        settings_copy = Object.assign({}, settings);
-    }
-    ctx.fillStyle = "#000";
-    ctx.fillRect(0, 0, can.width, can.height);
-    for (var i = 0; i < settings.TOTAL_CIRCLES; ++i) {
-        arr[i].update(i);
-        settings.show && arr[i].draw();
-    }
-    for (var i = 0; i <= can.width; i += settings.GRID_SIZE) {
-        for (var j = 0; j <= can.height; j += settings.GRID_SIZE) {
-            var v = 0;
-            for (var k = 0; k < settings.TOTAL_CIRCLES; ++k) {
-                v += (arr[k].radius * arr[k].radius) / (((arr[k].x - i) * (arr[k].x - i)) + ((arr[k].y - j) * (arr[k].y - j)))
-            }
-            grid[(i / settings.GRID_SIZE)][(j / settings.GRID_SIZE)] = v;
+    try {
+        if (JSON.stringify(settings_copy) != JSON.stringify(settings)) {
+            setup();
+            settings_copy = Object.assign({}, settings);
         }
-    }
-    var threshold = 1.0;
-    for (var i = 0; i < can.width - settings.GRID_SIZE; i += settings.GRID_SIZE) {
-        for (var j = 0; j < can.height - settings.GRID_SIZE; j += settings.GRID_SIZE) {
-            var a = grid[i / settings.GRID_SIZE][j / settings.GRID_SIZE] >= threshold;
-            var b = grid[i / settings.GRID_SIZE][(j + settings.GRID_SIZE) / settings.GRID_SIZE] >= threshold;
-            var c = grid[(i + settings.GRID_SIZE) / settings.GRID_SIZE][(j + settings.GRID_SIZE) / settings.GRID_SIZE] >= threshold;
-            var d = grid[(i + settings.GRID_SIZE) / settings.GRID_SIZE][j / settings.GRID_SIZE] >= threshold;
-            if (a + b + c + d == 0 || a + b + c + d == 4) continue;
-            var ap = {
-                x: i,
-                y: j
-            }, bp = {
-                x: i,
-                y: j + settings.GRID_SIZE
-            }, cp = {
-                x: i + settings.GRID_SIZE,
-                y: j + settings.GRID_SIZE
-            }, dp = {
-                x: i + settings.GRID_SIZE,
-                y: j
-            };
-            var p1, p2, p3, p4, temp;
-            var v = v_to_b(a, b, c, d);
-            if (v == 8 || v == 7) {
-                temp = lerp(ap, dp);
-                p1 = {
-                    x: temp.x,
-                    y: temp.y
-                };
-                temp = lerp(ap, bp);
-                p2 = {
-                    x: temp.x,
-                    y: temp.y
-                };
-            }
-            else if (v == 4 || v == 11) {
-                temp = lerp(bp, ap);
-                p1 = {
-                    x: temp.x,
-                    y: temp.y
-                };
-                temp = lerp(bp, cp)
-                p2 = {
-                    x: temp.x,
-                    y: temp.y
-                };
-            }
-            else if (v == 2 || v == 13) {
-                temp = lerp(cp, bp)
-                p1 = {
-                    x: temp.x,
-                    y: temp.y
-                };
-                temp = lerp(cp, dp)
-                p2 = {
-                    x: temp.x,
-                    y: temp.y
-                };
-            }
-            else if (v == 1 || v == 14) {
-                temp = lerp(dp, cp)
-                p1 = {
-                    x: temp.x,
-                    y: temp.y
-                };
-                temp = lerp(dp, ap);
-                p2 = {
-                    x: temp.x,
-                    y: temp.y
-                };
-            }
-            else if (v == 12 || v == 3) {
-                temp = lerp(ap, dp);
-                p1 = {
-                    x: temp.x,
-                    y: temp.y
-                };
-                temp = lerp(bp, cp)
-                p2 = {
-                    x: temp.x,
-                    y: temp.y
-                };
-            }
-            else if (v == 9 || v == 6) {
-                temp = lerp(ap, bp);
-                p1 = {
-                    x: temp.x,
-                    y: temp.y
-                };
-                temp = lerp(dp, cp);
-                p2 = {
-                    x: temp.x,
-                    y: temp.y
-                };
-            }
-            else if (v == 5) {
-                temp = lerp(ap, bp);
-                p1 = {
-                    x: temp.x,
-                    y: temp.y
-                };
-                temp = lerp(ap, dp);
-                p2 = {
-                    x: temp.x,
-                    y: temp.y
-                };
-                temp = lerp(bp, cp);
-                p3 = {
-                    x: temp.x,
-                    y: temp.y
-                };
-                temp = lerp(cp, dp);
-                p4 = {
-                    x: temp.x,
-                    y: temp.y
-                };
-            } else if (v == 10) {
-                temp = lerp(ap, bp);
-                p1 = {
-                    x: temp.x,
-                    y: temp.y
-                };
-                temp = lerp(bp, cp);
-                p2 = {
-                    x: temp.x,
-                    y: temp.y
-                };
-                temp = lerp(ap, dp);
-                p3 = {
-                    x: temp.x,
-                    y: temp.y
-                };
-                temp = lerp(dp, cp);
-                p4 = {
-                    x: temp.x,
-                    y: temp.y
-                };
-            }
-            if (p1 != undefined && p2 != undefined) {
-                ctx.beginPath();
-                ctx.strokeStyle = "orange";
-                ctx.moveTo(p1.x, p1.y);
-                ctx.lineTo(p2.x, p2.y);
-                ctx.stroke();
-                ctx.closePath();
-            }
-            if (p3 != undefined && p4 != undefined) {
-                ctx.beginPath();
-                ctx.strokeStyle = "orange";
-                ctx.moveTo(p3.x, p3.y);
-                ctx.lineTo(p4.x, p4.y);
-                ctx.stroke();
-                ctx.closePath();
+        ctx.fillStyle = "#000";
+        ctx.fillRect(0, 0, can.width, can.height);
+        for (var i = 0; i < settings.circles; ++i) {
+            arr[i].update(i);
+            settings.show && arr[i].draw();
+        }
+        for (var i = 0; i <= can.width; i += settings["grid size"]) {
+            for (var j = 0; j <= can.height; j += settings["grid size"]) {
+                var v = 0;
+                for (var k = 0; k < settings.circles; ++k) {
+                    v += (arr[k].radius * arr[k].radius) / (((arr[k].x - i) * (arr[k].x - i)) + ((arr[k].y - j) * (arr[k].y - j)))
+                }
+                grid[(i / settings["grid size"])][(j / settings["grid size"])] = v;
             }
         }
-    }
+        var threshold = 1.0;
+        for (var i = 0; i < can.width - settings["grid size"]; i += settings["grid size"]) {
+            for (var j = 0; j < can.height - settings["grid size"]; j += settings["grid size"]) {
+                var a = grid[i / settings["grid size"]][j / settings["grid size"]] >= threshold;
+                var b = grid[i / settings["grid size"]][(j + settings["grid size"]) / settings["grid size"]] >= threshold;
+                var c = grid[(i + settings["grid size"]) / settings["grid size"]][(j + settings["grid size"]) / settings["grid size"]] >= threshold;
+                var d = grid[(i + settings["grid size"]) / settings["grid size"]][j / settings["grid size"]] >= threshold;
+                if (a + b + c + d == 0 || a + b + c + d == 4) continue;
+                var ap = {
+                    x: i,
+                    y: j
+                }, bp = {
+                    x: i,
+                    y: j + settings["grid size"]
+                }, cp = {
+                    x: i + settings["grid size"],
+                    y: j + settings["grid size"]
+                }, dp = {
+                    x: i + settings["grid size"],
+                    y: j
+                };
+                var p1, p2, p3, p4, temp;
+                var v = v_to_b(a, b, c, d);
+                if (v == 8 || v == 7) {
+                    temp = lerp(ap, dp);
+                    p1 = {
+                        x: temp.x,
+                        y: temp.y
+                    };
+                    temp = lerp(ap, bp);
+                    p2 = {
+                        x: temp.x,
+                        y: temp.y
+                    };
+                }
+                else if (v == 4 || v == 11) {
+                    temp = lerp(bp, ap);
+                    p1 = {
+                        x: temp.x,
+                        y: temp.y
+                    };
+                    temp = lerp(bp, cp)
+                    p2 = {
+                        x: temp.x,
+                        y: temp.y
+                    };
+                }
+                else if (v == 2 || v == 13) {
+                    temp = lerp(cp, bp)
+                    p1 = {
+                        x: temp.x,
+                        y: temp.y
+                    };
+                    temp = lerp(cp, dp)
+                    p2 = {
+                        x: temp.x,
+                        y: temp.y
+                    };
+                }
+                else if (v == 1 || v == 14) {
+                    temp = lerp(dp, cp)
+                    p1 = {
+                        x: temp.x,
+                        y: temp.y
+                    };
+                    temp = lerp(dp, ap);
+                    p2 = {
+                        x: temp.x,
+                        y: temp.y
+                    };
+                }
+                else if (v == 12 || v == 3) {
+                    temp = lerp(ap, dp);
+                    p1 = {
+                        x: temp.x,
+                        y: temp.y
+                    };
+                    temp = lerp(bp, cp)
+                    p2 = {
+                        x: temp.x,
+                        y: temp.y
+                    };
+                }
+                else if (v == 9 || v == 6) {
+                    temp = lerp(ap, bp);
+                    p1 = {
+                        x: temp.x,
+                        y: temp.y
+                    };
+                    temp = lerp(dp, cp);
+                    p2 = {
+                        x: temp.x,
+                        y: temp.y
+                    };
+                }
+                else if (v == 5) {
+                    temp = lerp(ap, bp);
+                    p1 = {
+                        x: temp.x,
+                        y: temp.y
+                    };
+                    temp = lerp(ap, dp);
+                    p2 = {
+                        x: temp.x,
+                        y: temp.y
+                    };
+                    temp = lerp(bp, cp);
+                    p3 = {
+                        x: temp.x,
+                        y: temp.y
+                    };
+                    temp = lerp(cp, dp);
+                    p4 = {
+                        x: temp.x,
+                        y: temp.y
+                    };
+                } else if (v == 10) {
+                    temp = lerp(ap, bp);
+                    p1 = {
+                        x: temp.x,
+                        y: temp.y
+                    };
+                    temp = lerp(bp, cp);
+                    p2 = {
+                        x: temp.x,
+                        y: temp.y
+                    };
+                    temp = lerp(ap, dp);
+                    p3 = {
+                        x: temp.x,
+                        y: temp.y
+                    };
+                    temp = lerp(dp, cp);
+                    p4 = {
+                        x: temp.x,
+                        y: temp.y
+                    };
+                }
+                if (p1 != undefined && p2 != undefined) {
+                    ctx.beginPath();
+                    ctx.strokeStyle = "orange";
+                    ctx.moveTo(p1.x, p1.y);
+                    ctx.lineTo(p2.x, p2.y);
+                    ctx.stroke();
+                    ctx.closePath();
+                }
+                if (p3 != undefined && p4 != undefined) {
+                    ctx.beginPath();
+                    ctx.strokeStyle = "orange";
+                    ctx.moveTo(p3.x, p3.y);
+                    ctx.lineTo(p4.x, p4.y);
+                    ctx.stroke();
+                    ctx.closePath();
+                }
+            }
+        }
+    } catch (e) { console.log(e) }
     requestAnimationFrame(draw);
 }
 function resize() {
@@ -248,7 +258,7 @@ function resize() {
 }
 window.onresize = resize;
 can.onmousemove = function (e) {
-    for (var i = 0; i < settings.TOTAL_CIRCLES; ++i) {
+    for (var i = 0; i < settings.circles; ++i) {
         function timeout(i) {
             setTimeout(function () {
                 if (i < window.arr.length)
