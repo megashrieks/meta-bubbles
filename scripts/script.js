cb7f14ea3dfdd0c8317d9a231fd68bd7281eac04
acnologia <shrikanthbudya@gmail.com>
2018-04-14 13:52:08 +0530

    minor changes.

--- scripts/script.js
+++ scripts/script.js
@@ -3,13 +3,22 @@ var ctx = can.getContext("2d");
 
 var arr = [];
 var grid = [];
-const TOTAL_CIRCLES = 15;
-const GRID_SIZE = 5;
+const drag = 75;
+var settings = {
+    GRID_SIZE: 5,
+    TOTAL_CIRCLES: 35,
+    delay: 50,
+    show: true
+}, settings_copy = {};
 class Circle {
     constructor() {
         this.x = 50 + ~~(Math.random() * (can.width - 100));
         this.y = 50 + ~~(Math.random() * (can.height - 100));
-        this.radius = 40 + ~~(Math.random() * 20);
+        this.radius = 10 + ~~(Math.random() * 20);
+        this.target = {
+            x: can.width / 2,
+            y: can.height / 2
+        };
         this.directions = [1, -1]
         this.vel = {
             x: 3 * Math.random() * this.directions[~~(Math.random() * 2)],
@@ -17,31 +26,38 @@ class Circle {
         };
     }
     update() {
+        this.vel.x = (-this.x + this.target.x) / (drag * (this.radius / 15) / 10);
+        this.vel.y = (-this.y + this.target.y) / (drag * (this.radius / 15) / 10);
         this.x += this.vel.x;
         this.y += this.vel.y;
-        if (this.x + this.radius >= can.width || this.x - this.radius <= 0)
-            this.vel.x *= -1;
-        if (this.y + this.radius >= can.height || this.y - this.radius <= 0)
-            this.vel.y *= -1;
+    }
+    draw() {
+        ctx.beginPath();
+        ctx.strokeStyle = "#fff";
+        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI * 2);
+        ctx.stroke();
+        ctx.closePath();
     }
 }
 
 function setup() {
-    for (var i = 0; i < TOTAL_CIRCLES; ++i) {
+    // arr = [];
+    grid = [];
+    for (var i = arr.length; i < settings.TOTAL_CIRCLES; ++i) {
         var circ = new Circle();
         arr.push(circ);
     }
-    for (var i = 0; i < can.width; i += GRID_SIZE) {
+    for (var i = 0; i < can.width; i += settings.GRID_SIZE) {
         grid.push([]);
-        for (var j = 0; j < can.height; j += GRID_SIZE) {
-            grid[i / GRID_SIZE].push(0.0);
+        for (var j = 0; j < can.height; j += settings.GRID_SIZE) {
+            grid[i / settings.GRID_SIZE].push(0.0);
         }
     }
 }
 function lerp(p1, p2) {
     return {
-        x: p2.x + (p1.x - p2.x) * ((1.0 - grid[p2.x / GRID_SIZE][p2.y / GRID_SIZE]) / (grid[p1.x / GRID_SIZE][p1.y / GRID_SIZE] - grid[p2.x / GRID_SIZE][p2.y / GRID_SIZE])),
-        y: p2.y + (p1.y - p2.y) * ((1.0 - grid[p2.x / GRID_SIZE][p2.y / GRID_SIZE]) / (grid[p1.x / GRID_SIZE][p1.y / GRID_SIZE] - grid[p2.x / GRID_SIZE][p2.y / GRID_SIZE]))
+        x: p2.x + (p1.x - p2.x) * ((1.0 - grid[p2.x / settings.GRID_SIZE][p2.y / settings.GRID_SIZE]) / (grid[p1.x / settings.GRID_SIZE][p1.y / settings.GRID_SIZE] - grid[p2.x / settings.GRID_SIZE][p2.y / settings.GRID_SIZE])),
+        y: p2.y + (p1.y - p2.y) * ((1.0 - grid[p2.x / settings.GRID_SIZE][p2.y / settings.GRID_SIZE]) / (grid[p1.x / settings.GRID_SIZE][p1.y / settings.GRID_SIZE] - grid[p2.x / settings.GRID_SIZE][p2.y / settings.GRID_SIZE]))
     }
 }
 function v_to_b(v1, v2, v3, v4) {
@@ -49,40 +65,44 @@ function v_to_b(v1, v2, v3, v4) {
     return v4 + (v3 << 1) + (v2 << 2) + (v1 << 3);
 }
 function draw() {
+    if (JSON.stringify(settings_copy) != JSON.stringify(settings)) {
+        setup();
+        settings_copy = Object.assign({}, settings);
+    }
     ctx.fillStyle = "#000";
     ctx.fillRect(0, 0, can.width, can.height);
-    for (var i = 0; i < TOTAL_CIRCLES; ++i) {
-        arr[i].update();
-        //arr[i].draw();
+    for (var i = 0; i < settings.TOTAL_CIRCLES; ++i) {
+        arr[i].update(i);
+        settings.show && arr[i].draw();
     }
-    for (var i = 0; i <= can.width; i += GRID_SIZE) {
-        for (var j = 0; j <= can.height; j += GRID_SIZE) {
+    for (var i = 0; i <= can.width; i += settings.GRID_SIZE) {
+        for (var j = 0; j <= can.height; j += settings.GRID_SIZE) {
             var v = 0;
-            for (var k = 0; k < TOTAL_CIRCLES; ++k) {
+            for (var k = 0; k < settings.TOTAL_CIRCLES; ++k) {
                 v += (arr[k].radius * arr[k].radius) / (((arr[k].x - i) * (arr[k].x - i)) + ((arr[k].y - j) * (arr[k].y - j)))
             }
-            grid[(i / GRID_SIZE)][(j / GRID_SIZE)] = v;
+            grid[(i / settings.GRID_SIZE)][(j / settings.GRID_SIZE)] = v;
         }
     }
     var threshold = 1.0;
-    for (var i = 0; i < can.width - GRID_SIZE; i += GRID_SIZE) {
-        for (var j = 0; j < can.height - GRID_SIZE; j += GRID_SIZE) {
-            var a = grid[i / GRID_SIZE][j / GRID_SIZE] >= threshold;
-            var b = grid[i / GRID_SIZE][(j + GRID_SIZE) / GRID_SIZE] >= threshold;
-            var c = grid[(i + GRID_SIZE) / GRID_SIZE][(j + GRID_SIZE) / GRID_SIZE] >= threshold;
-            var d = grid[(i + GRID_SIZE) / GRID_SIZE][j / GRID_SIZE] >= threshold;
+    for (var i = 0; i < can.width - settings.GRID_SIZE; i += settings.GRID_SIZE) {
+        for (var j = 0; j < can.height - settings.GRID_SIZE; j += settings.GRID_SIZE) {
+            var a = grid[i / settings.GRID_SIZE][j / settings.GRID_SIZE] >= threshold;
+            var b = grid[i / settings.GRID_SIZE][(j + settings.GRID_SIZE) / settings.GRID_SIZE] >= threshold;
+            var c = grid[(i + settings.GRID_SIZE) / settings.GRID_SIZE][(j + settings.GRID_SIZE) / settings.GRID_SIZE] >= threshold;
+            var d = grid[(i + settings.GRID_SIZE) / settings.GRID_SIZE][j / settings.GRID_SIZE] >= threshold;
             if (a + b + c + d == 0 || a + b + c + d == 4) continue;
             var ap = {
                 x: i,
                 y: j
             }, bp = {
                 x: i,
-                y: j + GRID_SIZE
+                y: j + settings.GRID_SIZE
             }, cp = {
-                x: i + GRID_SIZE,
-                y: j + GRID_SIZE
+                x: i + settings.GRID_SIZE,
+                y: j + settings.GRID_SIZE
             }, dp = {
-                x: i + GRID_SIZE,
+                x: i + settings.GRID_SIZE,
                 y: j
             };
             var p1, p2, p3, p4, temp;
@@ -227,6 +247,19 @@ function resize() {
     can.height = window.innerHeight;
 }
 window.onresize = resize;
+can.onmousemove = function (e) {
+    for (var i = 0; i < settings.TOTAL_CIRCLES; ++i) {
+        function timeout(i) {
+            setTimeout(function () {
+                if (i < window.arr.length)
+                    window.arr[i].target = {
+                        x: e.clientX,
+                        y: e.clientY
+                    }
+            }, i * settings.delay);
+        }
+        timeout(i);
+    }
+};
 resize();
-setup();
 requestAnimationFrame(draw);
